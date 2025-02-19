ff8fbd97b5e19771384c8270ea304235f5614f40
Alexander <alexander@makklays.com.ua>
2025-02-19 22:21:38 +0100

    19-02-2025 Add new RegistrationController.

---
+++ src/main/java/com/techmatrix18/Controllers/api/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.techmatrix18.Controllers.api;
+
+import com.techmatrix18.Model.MyUser;
+import com.techmatrix18.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(value = "/req/signup", consumes = "application/json")
+    public MyUser createUser(@RequestBody MyUser user) {
+        return userRepository.save(user);
+    }
+
+
+}
+
