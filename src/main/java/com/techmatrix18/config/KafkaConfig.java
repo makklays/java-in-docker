5bbd6080d5cb9d0f07e70698046eebef7d291989
Alexander <alexander@makklays.com.ua>
2025-08-16 21:14:25 +0200

    16-08-2025 Add new views 2 KafkaTemplates for 'KaflaProducer'.

--- src/main/java/com/techmatrix18/config/KafkaConfig.java
+++ src/main/java/com/techmatrix18/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.techmatrix18.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,12 +11,14 @@ public class KafkaConfig {
 
     // KafkaTemplate для строк
     @Bean
+    @Qualifier("kafkaStringTemplate")
     public KafkaTemplate<String, String> kafkaStringTemplate(ProducerFactory<String, String> factory) {
         return new KafkaTemplate<>(factory);
     }
 
     // KafkaTemplate для byte[]
     @Bean
+    @Qualifier("kafkaBytesTemplate")
     public KafkaTemplate<String, byte[]> kafkaBytesTemplate(ProducerFactory<String, byte[]> factory) {
         return new KafkaTemplate<>(factory);
     }
